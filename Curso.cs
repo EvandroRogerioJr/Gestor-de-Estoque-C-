28b741d749cd4ec7ce6699790e73643201b27469
Evandro Junior <evandrorogeriojunior@hotmail.com>
2023-01-17 10:16:41 -0300

    Gestor de Estoque em C#

---
+++ Curso.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    [System.Serializable]
+    class Curso : Produto, IEstoque
+    {
+        public string autor;
+        private int vagas;
+
+        public Curso(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.autor = autor;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine($"Adicionar vagas no curso {nome}");
+            Console.WriteLine("Digite a quantidade de vagas que você quer dar a entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vagas += entrada;
+            Console.WriteLine("Entrada Resgitrada ");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSairda()
+        {
+            Console.WriteLine($"Consumir vagas no curso {nome}");
+            Console.WriteLine("Digite a quantidade de vagas que você quer consumir: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vagas -= entrada;
+            Console.WriteLine("Saida Resgitrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Vagas restantes: {vagas}");
+            Console.WriteLine($"Autor do Curso: {autor}");
+            Console.WriteLine("===============================");
+        }
+    }
+}
